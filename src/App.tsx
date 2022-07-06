8d03fc6d54ea2fdf154a35dcd1242c222cddefa5
N4n3x <alex.hern.dev@gmail.com>
2022-07-06 17:44:47 +0200

    implementation of PixiJS App in a ReactJS component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Game from './components/game'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,36 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <h1>⚔️ BEATS RPG 🛡️</h1>
       </header>
+      <Game />
     </div>
   )
 }
---
+++ src/components/game.tsx
@@ -0,0 +1,38 @@
+import { Application, Sprite } from 'pixi.js'
+import React, { useRef, useEffect, useLayoutEffect } from "react";
+
+const app = new Application({
+    width: 360,
+    height: 640,
+    antialias: true,
+    resolution: 1,
+    view: document.getElementById('pixi-canvas') as HTMLCanvasElement,
+});
+
+function Game() {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
+        // On first render add app to DOM
+        if (ref.current !== null) {
+            ref.current.appendChild(app.view);
+        }
+        // Start the PixiJS app
+        app.start();
+    
+        return () => {
+          // On unload stop the application
+          app.stop();
+        };
+      }, []);
+
+    
+    const sprite: Sprite = Sprite.from('../../static/hat-guy.png');
+    sprite.anchor.set(0.5);
+    sprite.x = app.screen.width / 2;
+    sprite.y = app.screen.height / 2;
+    app.stage.addChild(sprite);
+  return <div ref={ref}/>;
+}
+
+export default Game;
